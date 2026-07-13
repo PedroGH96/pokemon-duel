b35971551bcbf8d2a7a2dd89638fc9b7fdfc1dbe
PedroGH96 <pdalves30@gmail.com>
2026-07-13 17:04:46 -0300

    Reorganiza projeto: remove cache do Godot, backend duplicado obsoleto e organiza build web

--- backend/src/main/java/com/pokemonduel/model/enums/PokemonType.java
+++
@@ -1,9 +0,0 @@
-package com.pokemonduel.model.enums;
-
-/** Tipo elemental do Pokémon (apenas informativo nesta versão). */
-public enum PokemonType {
-    NORMAL, FOGO, AGUA, GRAMA, ELETRICO, GELO,
-    LUTADOR, PSIQUICO, FANTASMA, DRAGAO, VENENO,
-    TERRENO, VOADOR, INSETO, PEDRA, ROCHA, ACO,
-    SOMBRIO, FADA
-}
